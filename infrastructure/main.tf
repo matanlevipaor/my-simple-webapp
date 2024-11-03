1ab3a29ef70bbe112ed385b854b67b15c068542c
matanlp <paormatan@gmail.com>
2024-11-03 23:08:21 +0200

    Added Service Principal authentication for Terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,8 +9,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_app_service_plan" "asp" {
-  name                = "myAppServicePlan"
-  location            = azurerm_resource_group.rg.location
+  name = "myAppServicePlan"
+  location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku {
     tier = "Free"
